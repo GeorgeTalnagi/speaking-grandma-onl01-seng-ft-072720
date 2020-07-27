51f6b1de66974d58cc364d73934887f8177e54b6
George Talnagi <gtalnagi@yahoo.com>
2020-07-27 23:04:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,10 @@
+phrase = "I LOVE YOU GRANDMA!"
+
 def speak_to_grandma(phrase)
   if speak_to_grandma != (phrase.upcase)
-    return "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
   elsif speak_to_grandma == (phrase.upcase)
-    return "NO, NOT SINCE 1938!"
-  else speak_to_grandma == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "I LOVE YOU TOO PUMPKIN!"
 end
